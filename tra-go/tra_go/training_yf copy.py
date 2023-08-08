0bf9534660ef9b3fea0a374ed877a9a3de9b7c55
SWARAJ BISANE <bisane.s@SWARAJs-MacBook-Air.local>
2023-08-09 01:47:04 +0530

    new file name, training data class made for yahoo finance data, data clean function done.

--- tra-go/tra_go/training_yf copy.py
+++ tra-go/tra_go/training_yf copy.py
@@ -5,6 +5,8 @@ import pandas as pd
 from matplotlib import pyplot as plt
 from datetime import datetime, timedelta
 from time import time
+import pytz
+from copy import deepcopy
 
 
 class MyANN:
@@ -33,6 +35,9 @@ class MyANN:
 
     def to_date(self, datetime_1) -> str:
         return datetime.strptime(datetime_1, "%Y-%m-%d %H:%M:%S%z").date()
+
+    def to_date_str(self, datetime_1) -> str:
+        return self.to_date(datetime_1).strftime("%Y-%m-%d")
 
     def is_same_date(self, datetime_1, check_datetime):
         return self.to_date(datetime_1) == self.to_date(check_datetime)
@@ -91,7 +96,7 @@ class MyANN:
             ]
         ]
 
-    def get_data_all_df(self, which_half: str) -> pd.DataFrame:
+    def get_data_all_df(self) -> pd.DataFrame:
         df = pd.read_csv(self.get_csv_file_path())
 
         df["open"] = df["Open"].apply(lambda x: self.round_decimals_2(x))
@@ -115,84 +120,109 @@ class MyANN:
 
     def data_clean(
         self,
-    ) -> list:
-        # list containing dict key=data, data= day's dataframe.
-
-        # check all dates in the df
-        # then clean - if a datetime entry is missing or the value in 4 price columns is not there, put average values.
-        # full zone
-        # separate
-
-        # res = [{"2000-01-01": df_that_day}]
-
-        res = []
-        df = self.get_data_all_df(which_half="full_zone")
-        print(df)
-
-        df_sorted = df.sort_values(by="Datetime", ascending=True)
-
-        datetime_prev = datetime.strptime(
-            df_sorted.loc[0]["Datetime"], "%Y-%m-%d %H:%M:%S%z"
-        )
-
-        all_dates = []
-        for index, row in df_sorted.iterrows():
-            date_new = datetime.strptime(
-                row["Datetime"], "%Y-%m-%d %H:%M:%S%z"
-            ).strftime("%Y-%m-%d")
-            if date_new not in all_dates:
-                all_dates.append(date_new)
-
-        all_datetimes = []
+    ) -> pd.DataFrame:
         # start time = 0915
-        # lasat time = 1529
+        # last time = 1529
         # total minutes = 375
+
+        df = self.get_data_all_df()
+        df = df.sort_values(by="Datetime", ascending=True)
+
+        df["Dates"] = df["Datetime"].apply(lambda x: self.to_date_str(x))
+        all_dates = df["Dates"].unique()
+
+        all_datetimes_required = []
+        timezone = pytz.timezone("Asia/Kolkata")
         for date in all_dates:
-            datetime_new = datetime.strptime(date, "%Y-%m-%d")
-            datetime_1 = datetime_1 + timedelta(minutes=1)
+            that_date = datetime.strptime(date, "%Y-%m-%d")
+            date_obj = datetime(
+                year=that_date.year,
+                month=that_date.month,
+                day=that_date.day,
+                hour=9,
+                minute=15,
+                second=0,
+            )
+            first_datetime = timezone.localize(date_obj)
+            for i in range(375):
+                all_datetimes_required.append(
+                    (first_datetime + timedelta(minutes=i)).strftime(
+                        "%Y-%m-%d %H:%M:%S%z"
+                    )
+                )
 
-        # NOTE: assuming that 915 datetime exists for all days for all stocks.
-        # for index, row in df_sorted.iterrows():
-        #     first_minute = (
-        #         datetime.strptime(row["Datetime"], "%Y-%m-%d %H:%M:%S%z")
-        #         .time()
-        #         .strftime("%H:%M:%S")
-        #     )
-        #     if first_minute != "09:15:00":
-        #         datetime_prev_plus_1 = datetime_prev + timedelta(minutes=1)
-        #         if not self.is_same_date(
-        #             row["Datetime"],
-        #             datetime_prev_plus_1.strftime("%Y-%m-%d %H:%M:%S%z"),
-        #         ):
-        #             print(row["Datetime"])
-        #             df.loc[df_sorted.index[0]] = df_sorted.iloc[0]
-        #             print(df_sorted.iloc[0])
-        #     else:
+        all_datetimes_in_data = []
+        for index, row in df.iterrows():
+            all_datetimes_in_data.append(
+                datetime.strptime(row["Datetime"], "%Y-%m-%d %H:%M:%S%z").strftime(
+                    "%Y-%m-%d %H:%M:%S%z"
+                )
+            )
 
-        # df_sorted = df.sort_values(by="Datetime", ascending=True)
+        # make a set of all datetime to be there
+        # and what datetime are actually there
+        # finding missing ones
+        # add them as zeros in correct datetimes, sort df
+        # put previous values in them if not there.
 
-        # # Sort the DataFrame based on the 'Age' column in ascending order
-        # df_sorted = df.sort_values(by='Age', ascending=True)
+        all_datetimes_required_set = set(all_datetimes_required)
+        all_datetimes_in_data_set = set(all_datetimes_in_data)
 
-        # print(df_sorted)
+        missing_datetimes = all_datetimes_required_set - all_datetimes_in_data_set
 
-        # for datetime in df[["Datetime"]].values:
-        #     if self.is_in_zone(datetime[0]):
-        #         date_now = self.to_date(datetime[0])
-        #         if date_now not in data.keys():
-        #             data[date_now] = 0
-        #         data[date_now] += 1
+        add_df_rows = []
+        for d in missing_datetimes:
+            dict_1 = {
+                "Datetime": d,
+                "open": 0,
+                "close": 0,
+                "high": 0,
+                "low": 0,
+            }
+            add_df_rows.append(deepcopy(dict_1))
+            dict_1.clear()
 
-        # for i in data.keys():
-        #     print(i, "\t", data[i] - 264)
+        new_df = pd.DataFrame(add_df_rows)
+        df = pd.concat([df, new_df], ignore_index=True)
+        df = df.sort_values(by="Datetime", ascending=True)
+        print(df.columns)
+        df.drop("Dates", axis=1, inplace=True)
+        print(df.columns)
 
-        return res
+        missing_indexes = []
+        for index, row in df.iterrows():
+            if row["open"] == 0:
+                missing_indexes.append(index)
+
+        missing_rows = len(missing_indexes)
+        while missing_rows != 0:
+            for index in missing_indexes:
+                if df.at[index, "open"] == 0:
+                    if index > 0:
+                        ref_index = index - 1
+                    else:
+                        ref_index = index + 1
+
+                    if df.at[ref_index, "open"] != 0:
+                        df.at[index, "open"] = df.at[ref_index, "open"]
+                        df.at[index, "close"] = df.at[ref_index, "close"]
+                        df.at[index, "high"] = df.at[ref_index, "high"]
+                        df.at[index, "low"] = df.at[ref_index, "low"]
+                        missing_rows -= 1
+                        print(df.at[index, "Datetime"])
+
+        # then divide accordingly into 2 zones
+
+        return df
 
     def train_test_split(
         self,
         test_size_proportion: float,
     ) -> [pd.DataFrame]:
         # separate into 132, 132 entries df. for train and test df.
+
+        # divide the price data of that day by the cloding price of the previous day.
+        # for the very first day of the dataset - divide the prices by the opening price.
 
         list_df = self.data_clean()
 
