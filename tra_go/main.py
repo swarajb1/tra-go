9cc091290ee434a34e8345166705342eb328eac5
SWARAJ Mac <bisane.swaraj@gmail.com>
2023-11-18 17:04:51 +0530

    added a new band_2 model

--- tra_go/main.py
+++ tra_go/main.py
@@ -15,15 +15,7 @@ import keras_model as km
 import training_yf as an
 
 
-# nice -n 19 python file.py
-
-# /Users/bisane.s/my_files/my_codes/tra-go/.venv/bin/python /Users/bisane.s/my_files/my_codes/tra-go/tra_go/main.py
-
-# nice -n 19 /Users/bisane.s/my_files/my_codes/tra-go/.venv/bin/python tra_go/main.py
-
-
 # 2_mods = 2 hl models
-# commands
 # terminal command: tensorboard --logdir=training/logs/
 
 
@@ -32,16 +24,15 @@ BATCH_SIZE: int = 128
 LEARNING_RATE: float = 0.0001
 TEST_SIZE: float = 0.2
 
-Y_TYPE: str = "band"
-# Y_TYPE: str = "2_mods"
+Y_TYPE: str = "band_2"
 
-# Y_TYPE = "2_mods" / "band"
+# Y_TYPE = "2_mods" / "band" / band_2
 
 
 TICKER: str = "CCI"
 INTERVAL: str = "1m"
 
-IS_TRAINING_MODEL: bool = True
+IS_TRAINING_MODEL: bool = False
 
 PREV_MODEL_TRAINING: bool = False
 
@@ -49,7 +40,7 @@ PREV_MODEL_TRAINING: bool = False
 def main():
     df = an.get_data_all_df(ticker=TICKER, interval=INTERVAL)
 
-    prev_model: str = "2023-11-16 16-48"
+    prev_model: str = "2023-11-18 16-40"
 
     num_cores: int = multiprocessing.cpu_count()
     # total cores = 8 in this mac.
@@ -189,7 +180,13 @@ def main():
             model.compile(
                 optimizer=optimizer,
                 loss=loss,
-                metrics=["mae", km.metric_rmse],
+                metrics=[
+                    "mae",
+                    km.metric_rmse,
+                    km.metric_band_error_average,
+                    km.metric_band_hl_correction,
+                    km.metric_band_inside_range,
+                ],
             )
 
             # callbacks = [tensorboard_callback, loss_diff_callback]
@@ -227,6 +224,86 @@ def main():
         an.custom_evaluate_safety_factor_band_2(
             X_test=X_test,
             Y_test=Y_test,
+            testsize=TEST_SIZE,
+            now_datetime=now_datetime,
+        )
+
+    elif Y_TYPE == "band_2":
+        (X_train, Y_train), (X_test, Y_test) = an.train_test_split(
+            data_df=df, test_size=TEST_SIZE, y_type=Y_TYPE, interval=INTERVAL
+        )
+
+        if IS_TRAINING_MODEL and not PREV_MODEL_TRAINING:
+            now_datetime = datetime.now().strftime("%Y-%m-%d %H-%M")
+        else:
+            now_datetime = prev_model
+
+        if IS_TRAINING_MODEL:
+            model = km.get_untrained_model(X_train=X_train, y_type=Y_TYPE)
+
+            print("training data shape\t", X_train.shape)
+            print("training elememt shape\t", X_train[0].shape)
+
+            print("model output shape\t", model.output_shape)
+
+            # Directory where you want to store the TensorBoard logs
+            log_dir = f"training/logs/{now_datetime} - {Y_TYPE}"
+
+            tensorboard_callback = TensorBoard(log_dir=log_dir, histogram_freq=1)
+            loss_diff_callback = km.LossDifferenceCallback(log_dir=log_dir)
+
+            optimizer = km.get_optimiser(learning_rate=LEARNING_RATE)
+
+            loss = km.custom_loss_band_2_2
+
+            model.compile(
+                optimizer=optimizer,
+                loss=loss,
+                metrics=[
+                    "mae",
+                    km.metric_rmse,
+                    km.metric_band_error_average_2,
+                    km.metric_band_hl_correction_2,
+                    km.metric_band_inside_range_2,
+                ],
+            )
+
+            # callbacks = [tensorboard_callback, loss_diff_callback]
+            callbacks = [tensorboard_callback]
+
+            model.fit(
+                x=X_train,
+                y=Y_train,
+                epochs=NUMBER_OF_EPOCHS,
+                batch_size=BATCH_SIZE,
+                workers=num_workers,
+                use_multiprocessing=True,
+                validation_data=(X_test, Y_test),
+                callbacks=callbacks,
+            )
+
+            model.save(f"training/models/model - {now_datetime} - {Y_TYPE}")
+
+            print("\nmodel : training done. \n")
+
+        print(X_train.shape, X_test.shape)
+        print(Y_train.shape, Y_test.shape)
+
+        X_test = np.append(X_train, X_test, axis=0)
+        Y_test = np.append(Y_train, Y_test, axis=0)
+        print(Y_test)
+        zeros = np.zeros((Y_test.shape[0], Y_test.shape[1], 2))
+        Y_test = np.concatenate((Y_test, zeros), axis=2)
+        print(Y_test)
+        print(Y_test.shape)
+
+        print(f"\n\nnow_datatime:\t{now_datetime}\n\n")
+        print("-" * 30)
+
+        an.custom_evaluate_safety_factor_band_2_2(
+            X_test=X_test,
+            Y_test=Y_test,
+            y_type=Y_TYPE,
             testsize=TEST_SIZE,
             now_datetime=now_datetime,
         )
