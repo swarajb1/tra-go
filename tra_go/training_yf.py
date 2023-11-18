29cb0becee74e569100f43ccb7f84d6a1f9ae211
SWARAJ Mac <bisane.swaraj@gmail.com>
2023-11-18 17:04:51 +0530

    added a new band_2 model

--- tra_go/training_yf.py
+++ tra_go/training_yf.py
@@ -376,6 +376,14 @@ def train_test_split(
         train_y = by_date_df_array(df_train_y[selected_columns_2])
         test_y = by_date_df_array(df_test_y[selected_columns_2])
 
+    elif y_type == "band_2":
+        # all x and y is transformed to avg_price ,band_height
+        train_y_temp = by_date_df_array(df_train_y[selected_columns_2])
+        test_y_temp = by_date_df_array(df_test_y[selected_columns_2])
+
+        train_y = array_transform_avg_band(train_y_temp)
+        test_y = array_transform_avg_band(test_y_temp)
+
     return (
         (train_x, train_y),
         (test_x, test_y),
@@ -446,7 +454,7 @@ def points_hl(df):
     return res
 
 
-def by_date_df_array(df) -> [pd.DataFrame]:
+def by_date_df_array(df: pd.DataFrame) -> np.array:
     res = []
     full_rows = []
     for index, row in df.iterrows():
@@ -461,7 +469,19 @@ def by_date_df_array(df) -> [pd.DataFrame]:
     return res_1
 
 
-def by_date_np_array(np_array) -> [pd.DataFrame]:
+def array_transform_avg_band(array: np.array) -> np.array:
+    res = np.zeros_like(array)
+    # index 0 is low, 1 is high
+
+    # average price
+    res[:, :, 0] = (array[:, :, 0] + array[:, :, 1]) / 2
+    # band height
+    res[:, :, 1] = array[:, :, 1] - array[:, :, 0]
+
+    return res
+
+
+def by_date_np_array(np_array) -> list[pd.DataFrame]:
     res = []
     full_rows = np_array.tolist()
 
@@ -658,7 +678,144 @@ def custom_evaluate_safety_factor_band_2(
     # [0] is low, [1] is high
     # y_pred shape is (days, minutes, features)
 
-    first_percentile_exclude: float = 0.1
+    first_percentile_exclude: float = 0.01
+    first_point_taken: int = int(y_pred.shape[0] * first_percentile_exclude)
+    # make the y_pred, such that we first percentile array elements is same as the first precentile value
+    for i in range(y_pred.shape[0]):
+        for j in range(first_point_taken):
+            y_pred[i, j, 0] = y_pred[i, first_point_taken, 0]
+            y_pred[i, j, 1] = y_pred[i, first_point_taken, 1]
+    zeros = np.zeros((y_pred.shape[0], y_pred.shape[1], 2))
+    y_pred = np.concatenate((y_pred, zeros), axis=2)
+
+    list_min_pred = []
+    list_max_pred = []
+    list_min_actual = []
+    list_max_actual = []
+
+    for i in range(y_pred.shape[0]):
+        # i -> day
+        min_actual = min(Y_test[i, :, 0])
+        max_actual = max(Y_test[i, :, 1])
+
+        list_min_actual.append(min_actual)
+        list_max_actual.append(max_actual)
+
+    prev_val = -1
+    for safety_factor_i in [j / 20 for j in range(5, 21)]:
+        # safety_factor_i ranges from 0.25 to 0.95
+
+        for i in range(y_pred.shape[0]):
+            # i  -> day
+
+            all_y_pred_l = y_pred[i, :, 0]
+            all_y_pred_h = y_pred[i, :, 1]
+
+            min_pred = min(all_y_pred_l)
+            max_pred = max(all_y_pred_h)
+
+            average_pred = (min_pred + max_pred) / 2
+            min_t = average_pred + (min_pred - average_pred) * safety_factor_i
+            max_t = average_pred + (max_pred - average_pred) * safety_factor_i
+
+            list_min_pred.append(min_t)
+            list_max_pred.append(max_t)
+
+        val = function_make_win_graph(
+            list_max_actual=list_max_actual,
+            list_min_actual=list_min_actual,
+            list_max_pred=list_max_pred,
+            list_min_pred=list_min_pred,
+            testsize=testsize,
+            y_type=y_type,
+            max_percentile_found=False,
+            now_datetime=now_datetime,
+        )
+        if val > 0:
+            print("sf:", safety_factor_i, "{:0.6f}".format(val))
+        if val > prev_val:
+            safety_factor = safety_factor_i
+            prev_val = val
+
+        list_min_pred.clear()
+        list_max_pred.clear()
+
+    # safety factor found
+    if prev_val == 0:
+        safety_factor = 1
+
+    for i in range(y_pred.shape[0]):
+        # i  -> day
+
+        all_y_pred_l = y_pred[i, :, 0]
+        all_y_pred_h = y_pred[i, :, 1]
+
+        min_pred = min(all_y_pred_l)
+        max_pred = max(all_y_pred_h)
+
+        average_pred = (min_pred + max_pred) / 2
+        min_t = average_pred + (min_pred - average_pred) * safety_factor
+        max_t = average_pred + (max_pred - average_pred) * safety_factor
+
+        list_min_pred.append(min_t)
+        list_max_pred.append(max_t)
+
+    function_error_132_graph(y_pred=y_pred, y_test=Y_test, now_datetime=now_datetime, y_type=y_type)
+
+    print("\nmax_safety_factor\t", safety_factor, "\n")
+
+    function_make_win_graph(
+        list_max_actual=list_max_actual,
+        list_min_actual=list_min_actual,
+        list_max_pred=list_max_pred,
+        list_min_pred=list_min_pred,
+        testsize=testsize,
+        y_type=y_type,
+        max_percentile_found=True,
+        now_datetime=now_datetime,
+    )
+
+    return
+
+
+def custom_evaluate_safety_factor_band_2_2(
+    X_test,
+    Y_test,
+    testsize,
+    y_type: str,
+    now_datetime,
+):
+    safety_factor: float = 0.8
+
+    with custom_object_scope(
+        {
+            "custom_loss_band_2_2": km.custom_loss_band_2_2,
+            "metric_rmse": km.metric_rmse,
+            "metric_band_error_average_2": km.metric_band_error_average_2,
+            "metric_band_hl_correction_2": km.metric_band_hl_correction_2,
+            "metric_band_inside_range_2": km.metric_band_inside_range_2,
+        }
+    ):
+        model = keras.models.load_model(f"training/models/model - {now_datetime} - {y_type}")
+        model.summary()
+
+    y_pred = model.predict(X_test)
+    # transform y data from (0, 1) = (avg, band) to (low, high)
+
+    y_pred_temp = np.zeros_like(y_pred)
+    y_pred_temp[:, :, 0] = y_pred[:, :, 0] - y_pred[:, :, 1]
+    y_pred_temp[:, :, 1] = y_pred[:, :, 0] + y_pred[:, :, 1]
+    y_pred = y_pred_temp
+
+    Y_test_temp = np.zeros_like(Y_test)
+    Y_test_temp[:, :, 0] = Y_test[:, :, 0] - Y_test[:, :, 1]
+    Y_test_temp[:, :, 1] = Y_test[:, :, 0] + Y_test[:, :, 1]
+    Y_test = Y_test_temp
+
+    # [0] is low, [1] is high
+    # y_pred shape is (days, minutes, features)
+
+    first_percentile_exclude: float = 0.01
     first_point_taken: int = int(y_pred.shape[0] * first_percentile_exclude)
     # make the y_pred, such that we first percentile array elements is same as the first precentile value
     for i in range(y_pred.shape[0]):
@@ -1114,7 +1271,7 @@ def function_make_win_graph(
         print("NUMBER_OF_LAYERS\t\t", km.NUMBER_OF_LAYERS)
         print("INITIAL_DROPOUT\t\t\t", km.INITIAL_DROPOUT)
 
-        if y_type != "band":
+        if y_type != "band" or y_type != "band_2":
             print("ERROR_AMPLIFICATION_FACTOR\t", km.ERROR_AMPLIFICATION_FACTOR, end="\n\n")
 
     return pro_250
