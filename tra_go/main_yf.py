a3b502e0b436ab8502e92c6e97da10b531d06fff
SWARAJ Mac <bisane.swaraj@gmail.com>
2024-02-29 01:17:36 +0530

    new files and updates

--- tra_go/main_yf.py
+++ tra_go/main_yf.py
@@ -1,9 +1,10 @@
 import time
 from datetime import datetime
 
+# import band_2.training_yf_band_2 as an_2
 import band_4.keras_model_band_4 as km_4
 import keras_model as km
-import training_zero as an
+import training_yf as an
 from band_4.training_yf_band_4 import CustomEvaluation
 from keras.callbacks import ModelCheckpoint, TensorBoard, TerminateOnNaN
 
@@ -22,6 +23,8 @@ TICKER: str = "CCI"
 INTERVAL: str = "1m"
 
 PREV_MODEL_TRAINING: bool = False
+
+# terminal command: tensorboard --logdir=training/logs/
 
 
 def main():
@@ -76,28 +79,28 @@ def main():
             terNan = TerminateOnNaN()
 
             mcp_save_2 = ModelCheckpoint(
-                f"training/models/model - {now_datetime} - {Y_TYPE} - modelCheckPoint-2.keras",
+                f"training/models/model - {now_datetime} - {Y_TYPE} - modelCheckPoint-2",
                 save_best_only=True,
                 monitor="loss",
                 mode="min",
             )
 
             mcp_save_3 = ModelCheckpoint(
-                f"training/models/model - {now_datetime} - {Y_TYPE} - modelCheckPoint-3.keras",
+                f"training/models/model - {now_datetime} - {Y_TYPE} - modelCheckPoint-3",
                 save_best_only=True,
                 monitor="val_loss",
                 mode="min",
             )
 
             mcp_save_4 = ModelCheckpoint(
-                f"training/models/model - {now_datetime} - {Y_TYPE} - modelCheckPoint-4.keras",
+                f"training/models/model - {now_datetime} - {Y_TYPE} - modelCheckPoint-4",
                 save_best_only=True,
                 monitor="metric_win_checkpoint",
                 mode="max",
             )
 
             mcp_save_5 = ModelCheckpoint(
-                f"training/models/model - {now_datetime} - {Y_TYPE} - modelCheckPoint-5.keras",
+                f"training/models/model - {now_datetime} - {Y_TYPE} - modelCheckPoint-5",
                 save_best_only=True,
                 monitor="val_metric_win_checkpoint",
                 mode="max",
@@ -164,6 +167,107 @@ def main():
             test_size=0,
             now_datetime=now_datetime,
         )
+
+    # elif Y_TYPE == "band_5":
+    #     (X_train, Y_train, train_prev_close), (X_test, Y_test, test_prev_close) = an.train_test_split_5(
+    #         data_df=df,
+    #         test_size=TEST_SIZE,
+    #         y_type=Y_TYPE,
+    #         interval=INTERVAL,
+    #     )
+
+    #     if IS_TRAINING_MODEL and not PREV_MODEL_TRAINING:
+    #         now_datetime = datetime.now().strftime("%Y-%m-%d %H-%M")
+    #     else:
+    #         now_datetime = prev_model
+
+    #     if IS_TRAINING_MODEL:
+    #         model = km.get_untrained_model_5(X_train=X_train, y_type=Y_TYPE)
+
+    #         print("training data shape\t", X_train.shape)
+    #         print("training elememt shape\t", X_train[0].shape)
+
+    #         print("model output shape\t", model.output_shape)
+
+    #         optimizer = km.get_optimiser(learning_rate=LEARNING_RATE)
+
+    #         loss = km_4.metric_new_idea
+
+    #         model.compile(
+    #             optimizer=optimizer,
+    #             loss=loss,
+    #             metrics=[
+    #                 km_4.metric_abs_percent_5,
+    #                 km_4.metric_rmse_percent_5,
+    #                 km_4.metric_loss_comp_2,
+    #                 km_4.metric_win_percent,
+    #                 km_4.metric_pred_capture_percent,
+    #                 km_4.metric_win_pred_capture_percent,
+    #             ],
+    #         )
+
+    #         log_dir: str = f"training/logs/{now_datetime} - {Y_TYPE}"
+
+    #         tensorboard_callback = TensorBoard(log_dir=log_dir, histogram_freq=1)
+    #         terNan = TerminateOnNaN()
+
+    #         mcp_save = ModelCheckpoint(
+    #             f"training/models/model - {now_datetime} - {Y_TYPE} - modelCheckPoint",
+    #             save_best_only=True,
+    #             monitor="val_metric_rmse_percent_5",
+    #             mode="min",
+    #         )
+
+    #         mcp_save_3 = ModelCheckpoint(
+    #             f"training/models/model - {now_datetime} - {Y_TYPE} - modelCheckPoint-3",
+    #             save_best_only=True,
+    #             monitor="val_loss",
+    #             mode="min",
+    #         )
+
+    #         callbacks = [tensorboard_callback, terNan, mcp_save, mcp_save_3]
+
+    #         history = model.fit(
+    #             x=X_train,
+    #             y=Y_train,
+    #             epochs=NUMBER_OF_EPOCHS,
+    #             batch_size=BATCH_SIZE,
+    #             workers=num_workers,
+    #             use_multiprocessing=True,
+    #             validation_data=(X_test, Y_test),
+    #             callbacks=callbacks,
+    #         )
+
+    #         model.save(f"training/models/model - {now_datetime} - {Y_TYPE}")
+
+    #         print("\nmodel : training done. \n")
+
+    #     print(f"\n\nnow_datatime:\t{now_datetime}\n\n")
+    #     print("-" * 30)
+
+    #     print("\n" * 4, "*" * 80, "\n" * 4)
+    #     print("only training data now")
+
+    #     training_data_custom_evaluation = CustomEvaluation(
+    #         X_data=X_train,
+    #         Y_data=Y_train,
+    #         prev_close=train_prev_close,
+    #         y_type=Y_TYPE,
+    #         test_size=0,
+    #         now_datetime=now_datetime,
+    #     )
+
+    #     print("\n" * 4, "*" * 500, "\n" * 4)
+    #     print("only validation data now")
+
+    #     valid_data_custom_evaluation = CustomEvaluation(
+    #         X_data=X_test,
+    #         Y_data=Y_test,
+    #         prev_close=test_prev_close,
+    #         y_type=Y_TYPE,
+    #         test_size=0,
+    #         now_datetime=now_datetime,
+    #     )
 
 
 if __name__ == "__main__":
---
+++ tra_go/training_zero.py
@@ -0,0 +1,488 @@
+from copy import deepcopy
+from datetime import datetime, timedelta
+
+import numpy as np
+import pandas as pd
+import pytz
+
+
+class DataCleanerZero:
+    def __init__(self, df: pd.DataFrame, ticker: str, interval: str):
+        self.df = df
+        self.ticker = ticker
+        self.interval = interval
+
+
+def is_in_half(check_datetime, which_half: int, interval: str) -> bool:
+    # which_half = 0, means 1st half - input data
+    # which_half = 1, means 2nd half - predict data
+
+    datetime_1 = datetime.strptime("2000-01-01 10:00:00+0530", "%Y-%m-%d %H:%M:%S%z")
+    time_1 = (datetime_1 + timedelta(minutes=132 * which_half)).time()
+    time_check = datetime.strptime(check_datetime, "%Y-%m-%d %H:%M:%S%z").time()
+
+    if interval == "1m":
+        time_2 = (datetime_1 + timedelta(minutes=132 * (which_half + 1) + 1)).time()
+
+    elif interval == "2m":
+        time_2 = (datetime_1 + timedelta(minutes=132 * (which_half + 1) + 2)).time()
+
+    elif interval == "5m":
+        time_2 = (datetime_1 + timedelta(minutes=132 * (which_half + 1) + 5)).time()
+
+    if time_check > time_1 and time_check < time_2:
+        return True
+
+    return False
+
+
+def to_date(datetime_1) -> str:
+    return datetime.strptime(datetime_1, "%Y-%m-%d %H:%M:%S%z").date()
+
+
+def to_date_str(datetime_1) -> str:
+    return to_date(datetime_1).strftime("%Y-%m-%d")
+
+
+def is_same_date(datetime_1, check_datetime):
+    return to_date(datetime_1) == to_date(check_datetime)
+
+
+def is_same_date_2(date_1, list_check_date_str):
+    for d_1 in list_check_date_str:
+        if d_1 == date_1:
+            return True
+    return False
+
+
+def is_in_zone(check_datetime, interval) -> bool:
+    datetime_1 = datetime.strptime("2000-01-01 10:00:00+0530", "%Y-%m-%d %H:%M:%S%z")
+    time_1 = datetime_1.time()
+    time_check = datetime.strptime(check_datetime, "%Y-%m-%d %H:%M:%S%z").time()
+
+    if interval == "1m":
+        time_2 = (datetime_1 + timedelta(minutes=132 * 2 + 1)).time()
+
+    elif interval == "2m":
+        time_2 = (datetime_1 + timedelta(minutes=132 * 2 + 2)).time()
+
+    elif interval == "5m":
+        time_2 = (datetime_1 + timedelta(minutes=132 * 2 + 5)).time()
+
+    if time_check > time_1 and time_check < time_2:
+        return True
+    return False
+
+
+def is_in_first_half(check_datetime, interval):
+    return is_in_half(check_datetime, which_half=0, interval=interval)
+
+
+def is_in_second_half(check_datetime, interval):
+    return is_in_half(check_datetime, which_half=1, interval=interval)
+
+
+def get_data_df(ticker, interval, which_half: str) -> pd.DataFrame:
+    df = pd.read_csv(get_csv_file_path(ticker, interval))
+
+    if which_half == "full_zone":
+        df["to_add"] = df["Datetime"].apply(lambda x: is_in_zone(x, interval=interval))
+    elif which_half == "first_half":
+        df["to_add"] = df["Datetime"].apply(lambda x: is_in_first_half(x))
+    elif which_half == "second_half":
+        df["to_add"] = df["Datetime"].apply(lambda x: is_in_second_half(x))
+
+    df["open"] = df["open"].apply(lambda x: round(number=x, ndigits=2))
+    df["close"] = df["close"].apply(lambda x: round(number=x, ndigits=2))
+    df["high"] = df["high"].apply(lambda x: round(number=x, ndigits=2))
+    df["low"] = df["low"].apply(lambda x: round(number=x, ndigits=2))
+
+    new_2 = df[df["to_add"] is True].copy(deep=True)
+    new_2.rename(columns={"Datetime": "datetime"}, inplace=True)
+
+    return new_2[
+        [
+            "datetime",
+            "open",
+            "close",
+            "high",
+            "low",
+        ]
+    ]
+
+
+def get_data_all_df(ticker, interval) -> pd.DataFrame:
+    if ticker == "CCI":
+        ticker = "ICICIBANK"
+
+    df = pd.read_csv(get_csv_file_path(ticker, interval))
+
+    df["open"] = df["open"].apply(lambda x: round(number=x, ndigits=2))
+    df["close"] = df["close"].apply(lambda x: round(number=x, ndigits=2))
+    df["high"] = df["high"].apply(lambda x: round(number=x, ndigits=2))
+    df["low"] = df["low"].apply(lambda x: round(number=x, ndigits=2))
+
+    df.rename(columns={"date": "datetime"}, inplace=True)
+
+    return df[
+        [
+            "datetime",
+            "open",
+            "close",
+            "high",
+            "low",
+        ]
+    ]
+
+
+def get_csv_file_path(ticker, interval) -> str:
+    file_path = f"./new_data_z/{interval}_data/{ticker} - {interval}.csv"
+    return file_path
+
+
+def data_cleaning(df: pd.DataFrame) -> pd.DataFrame:
+    # start time = 0915
+    # last time = 1529
+    # total minutes = 375
+
+    df = df.sort_values(by="datetime", ascending=True)
+
+    df["date"] = df["datetime"].apply(lambda x: to_date_str(x))
+    all_dates = df["date"].unique()
+
+    all_datetimes_required = []
+    timezone = pytz.timezone("Asia/Kolkata")
+    for date in all_dates:
+        that_date = datetime.strptime(date, "%Y-%m-%d")
+        date_obj = datetime(
+            year=that_date.year,
+            month=that_date.month,
+            day=that_date.day,
+            hour=9,
+            minute=15,
+            second=0,
+        )
+        first_datetime = timezone.localize(date_obj)
+        for i in range(375):
+            all_datetimes_required.append(
+                (first_datetime + timedelta(minutes=i)).strftime("%Y-%m-%d %H:%M:%S%z"),
+            )
+
+    all_datetimes_in_data = []
+    for index, row in df.iterrows():
+        all_datetimes_in_data.append(
+            datetime.strptime(row["datetime"], "%Y-%m-%d %H:%M:%S%z").strftime(
+                "%Y-%m-%d %H:%M:%S%z",
+            ),
+        )
+
+    # make a set of all datetime to be there
+    # and what datetime are actually there
+    # finding missing ones, using set
+    # add them as zeros in correct datetimes, sort df
+    # put previous values in them in palce of zeros
+
+    all_datetimes_required_set = set(all_datetimes_required)
+    all_datetimes_in_data_set = set(all_datetimes_in_data)
+
+    missing_datetimes = all_datetimes_required_set - all_datetimes_in_data_set
+
+    add_df_rows = []
+    for d in missing_datetimes:
+        dict_1 = {
+            "datetime": d,
+            "open": 0,
+            "close": 0,
+            "high": 0,
+            "low": 0,
+        }
+        add_df_rows.append(deepcopy(dict_1))
+        dict_1.clear()
+
+    new_df = pd.DataFrame(add_df_rows)
+    df = pd.concat([df, new_df], ignore_index=True)
+
+    df = df.sort_values(by="datetime", ascending=True)
+    df.reset_index(drop=True, inplace=True)
+
+    df.drop("date", axis=1, inplace=True)
+
+    missing_indexes = []
+    for index, row in df.iterrows():
+        if row["open"] == 0:
+            missing_indexes.append(index)
+
+    missing_rows = len(missing_indexes)
+    while missing_rows != 0:
+        for index in missing_indexes:
+            if df.at[index, "open"] == 0:
+                if index > 0:
+                    ref_index = index - 1
+                else:
+                    ref_index = index + 1
+
+                if df.at[ref_index, "open"] != 0:
+                    df.at[index, "open"] = df.at[ref_index, "open"]
+                    df.at[index, "close"] = df.at[ref_index, "close"]
+                    df.at[index, "high"] = df.at[ref_index, "high"]
+                    df.at[index, "low"] = df.at[ref_index, "low"]
+                    missing_rows -= 1
+
+    # then divide accordingly into 2 zones
+    # send only full zone df, not full date.
+
+    return df[
+        [
+            "datetime",
+            "open",
+            "close",
+            "high",
+            "low",
+        ]
+    ]
+
+
+def data_scaling(df: pd.DataFrame) -> pd.DataFrame:
+    # fixded 375 rows for all days.
+    # just divide by the close value of previous row,
+    # for the very first day, use the open value
+
+    one_day = 375
+    num_days = len(df) // one_day
+
+    # for other days
+    for j in range(num_days - 1, 0, -1):
+        prev_close = df.loc[j * one_day - 1, "close"]
+        for i in range(one_day):
+            df.loc[j * one_day + i, "close"] /= prev_close
+            df.loc[j * one_day + i, "high"] /= prev_close
+            df.loc[j * one_day + i, "low"] /= prev_close
+            df.loc[j * one_day + i, "open"] /= prev_close
+
+    # for 1st day
+    open_val = df.at[0, "open"]
+    for i in range(one_day):
+        df.at[i, "close"] /= open_val
+        df.at[i, "high"] /= open_val
+        df.at[i, "low"] /= open_val
+        df.at[i, "open"] /= open_val
+
+    return df
+
+
+def get_prev_close(df: pd.DataFrame) -> np.ndarray:
+    one_day = 375
+    num_days = len(df) // one_day
+
+    res: np.ndarray = np.zeros(num_days)
+
+    # for other days
+    for j in range(num_days - 1, 0, -1):
+        prev_close = df.loc[j * one_day - 1, "close"]
+        res[j] = prev_close
+
+    # for 1st day
+    open_val = df.at[0, "open"]
+    res[0] = open_val
+
+    return res
+
+
+def data_my_zone(df: pd.DataFrame, interval) -> pd.DataFrame:
+    df["to_add"] = df["datetime"].apply(lambda x: is_in_zone(x, interval=interval))
+
+    new_2 = df[df["to_add"]].copy(deep=True)
+    new_2.reset_index(drop=True, inplace=True)
+
+    return new_2[
+        [
+            "datetime",
+            "open",
+            "close",
+            "high",
+            "low",
+        ]
+    ]
+
+
+def data_split_train_test(df: pd.DataFrame, test_size) -> pd.DataFrame:
+    # split into train and test
+    # not 4 parts, with x and y
+    # now both x, y in one df
+
+    df["date"] = df["datetime"].apply(lambda x: to_date_str(x))
+    all_dates = df["date"].unique()
+    num_days = len(all_dates)
+
+    training_dates = all_dates[: int(num_days * (1 - test_size))]
+    testing_dates = all_dates[int(num_days * (1 - test_size)) :]
+
+    df["is_train"] = df["date"].apply(lambda x: is_same_date_2(x, training_dates))
+
+    df["is_test"] = df["date"].apply(lambda x: is_same_date_2(x, testing_dates))
+
+    train_df = df[df["is_train"]].copy(deep=True)
+    test_df = df[df["is_test"]].copy(deep=True)
+
+    train_df.reset_index(drop=True, inplace=True)
+    test_df.reset_index(drop=True, inplace=True)
+
+    return (
+        train_df[["datetime", "open", "close", "high", "low", "date"]],
+        test_df[["datetime", "open", "close", "high", "low", "date"]],
+    )
+
+
+def data_split_x_y(df, interval) -> pd.DataFrame:
+    df["is_input"] = df["datetime"].apply(lambda x: is_in_half(x, which_half=0, interval=interval))
+    df["is_output"] = df["datetime"].apply(lambda x: is_in_half(x, which_half=1, interval=interval))
+
+    df_i = df[df["is_input"]].copy(deep=True)
+    df_o = df[df["is_output"]].copy(deep=True)
+
+    df_i.reset_index(drop=True, inplace=True)
+    df_o.reset_index(drop=True, inplace=True)
+
+    return (
+        df_i[["datetime", "open", "close", "high", "low", "date"]],
+        df_o[["datetime", "open", "close", "high", "low", "date"]],
+    )
+
+
+def data_split_x_y_new(df, interval) -> pd.DataFrame:
+    # split the whole dataframe into 2 parts = input and output
+
+    df["date"] = df["datetime"].apply(lambda x: to_date_str(x))
+
+    df["is_input"] = df["datetime"].apply(
+        lambda x: is_in_half(x, which_half=0, interval=interval),
+    )
+    df["is_output"] = df["datetime"].apply(
+        lambda x: is_in_half(x, which_half=1, interval=interval),
+    )
+
+    df_i = df[df["is_input"]].copy(deep=True)
+    df_o = df[df["is_output"]].copy(deep=True)
+
+    df_i.reset_index(drop=True, inplace=True)
+    df_o.reset_index(drop=True, inplace=True)
+
+    return (
+        df_i[["datetime", "open", "close", "high", "low", "date"]],
+        df_o[["datetime", "open", "close", "high", "low", "date"]],
+    )
+
+
+def train_test_split(data_df, interval, y_type, test_size=0.2) -> pd.DataFrame:
+    # separate into 132, 132 entries df. for train and test df.
+
+    # divide the price data of that day by the closing price of the previous day.
+    # for the very first day of the dataset - divide the prices by the opening price.
+
+    df = data_cleaning(data_df)
+
+    df_cleaned = df.copy(deep=True)
+
+    # getting clean and my zone data
+    df = data_scaling(df)
+    # getting scaled data according to previous day closing price, in percentages terms
+    df = data_my_zone(df, interval=interval)
+    # getting data is inside the full zone.
+
+    df_train, df_test = data_split_train_test(df=df, test_size=test_size)
+
+    df_train_x, df_train_y = data_split_x_y(df=df_train, interval=interval)
+    # 23x(132,4)
+
+    df_test_x, df_test_y = data_split_x_y(df=df_test, interval=interval)
+    # 6x(132,4)
+
+    selected_columns_1 = ["low", "high", "open", "close"]
+
+    train_x = by_date_df_array(df_train_x[selected_columns_1])
+    test_x = by_date_df_array(df_test_x[selected_columns_1])
+
+    train_y = by_date_df_array(df_train_y[selected_columns_1])
+    test_y = by_date_df_array(df_test_y[selected_columns_1])
+
+    df_train_c, df_test_c = data_split_train_test(df=df_cleaned, test_size=test_size)
+
+    train_prev_close = get_prev_close(df_train_c)
+    test_prev_close = get_prev_close(df_test_c)
+
+    return (
+        (train_x, train_y, train_prev_close),
+        (test_x, test_y, test_prev_close),
+    )
+
+
+def last_close_value(df: pd.DataFrame) -> np.ndarray:
+    res = np.array([])
+
+    res = np.append(res, df.iloc[0]["open"])
+
+    for i in range(1, len(df) // 132):
+        res = np.append(res, df.iloc[(i - 1) * 132 + 131]["close"])
+
+    return res
+
+
+def by_date_df_array(df: pd.DataFrame) -> np.ndarray:
+    res = []
+    full_rows = []
+    for index, row in df.iterrows():
+        x = row.values.tolist()
+        full_rows.append(x)
+
+    for i in range(len(full_rows) // 132):
+        res.append(deepcopy(full_rows[i * 132 : (i + 1) * 132]))
+
+    res_1 = np.array([np.array(xi) for xi in res])
+
+    return res_1
+
+
+def split_by_date(data_df: pd.DataFrame, interval: str, columns: list[str]):
+    """Assumes that the number of data points is divisible by 132."""
+
+    if interval == "1m":
+        number_of_points: int = 132
+
+    res: np.ndarray = np.empty((0, number_of_points, len(columns)))
+
+    temp = np.empty((0, len(columns)))
+    for index, row in data_df.iterrows():
+        z = row.loc[columns].values
+        temp = np.append(temp, [z], axis=0)
+
+        if (index + 1) % number_of_points == 0:
+            res = np.append(res, np.array([temp]), axis=0)
+            temp = np.empty((0, len(columns)))
+
+    return res
+
+
+def get_x_y_individual_data(
+    data_df: pd.DataFrame,
+    interval: str,
+    columns: list[str],
+) -> np.ndarray:
+    df = data_cleaning(data_df)
+    # getting clean and my zone data
+    df = data_scaling(df)
+    # getting scaled data according to previous day closing price, in percentages terms
+    df = data_my_zone(df, interval=interval)
+
+    data_df_x, data_df_y = data_split_x_y_new(df=df, interval=interval)
+
+    arr_x = split_by_date(data_df=data_df_x, interval=interval, columns=columns)
+    arr_y = split_by_date(data_df=data_df_y, interval=interval, columns=columns)
+
+    arr_x = arr_x.astype(np.float32)
+    arr_y = arr_y.astype(np.float32)
+
+    return arr_x, arr_y
+
+
+def round_to_nearest_0_05(value):
+    return np.round(value * 20) / 20
