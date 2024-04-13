7e45f3d7f615973dd18fe9459787753e2688a606
SWARAJ Mac <bisane.swaraj@gmail.com>
2024-04-14 04:13:00 +0530

    new changes

--- tra_go/keras_model.py
+++ tra_go/keras_model.py
@@ -8,43 +8,6 @@ INITIAL_DROPOUT_PERCENT: float = 0
 
 
 def get_untrained_model(X_train, y_type):
-    model = keras.Sequential()
-
-    model.add(
-        LSTM(
-            units=NUMBER_OF_NEURONS,
-            input_shape=(X_train[0].shape),
-            return_sequences=True,
-            activation="relu",
-        ),
-    )
-
-    model.add(Dropout(INITIAL_DROPOUT_PERCENT / 100))
-
-    for i in range(NUMBER_OF_LAYERS - 1):
-        model.add(
-            LSTM(
-                units=NUMBER_OF_NEURONS,
-                return_sequences=True,
-                activation="relu",
-            ),
-        )
-        #  dropout value decreases in exponential fashion.
-        model.add(Dropout(pow(INITIAL_DROPOUT_PERCENT, 1 / (i + 2)) / 100))
-
-    if y_type == "band_2":
-        model.add(Dense(2))
-
-    elif y_type == "band_4":
-        model.add(Dense(4))
-
-    model.summary()
-    print("\n" * 2)
-
-    return model
-
-
-def get_untrained_model_new(X_train):
     model = keras.models.Sequential()
 
     model.add(Input(shape=(X_train[0].shape)))
@@ -62,7 +25,11 @@ def get_untrained_model_new(X_train):
         #  dropout value decreases in exponential fashion.
         model.add(Dropout(pow(1 + INITIAL_DROPOUT_PERCENT / 100, 1 / (i + 1)) - 1))
 
-    model.add(Dense(4))
+    if y_type == "band_2":
+        model.add(Dense(2))
+
+    elif y_type == "band_4":
+        model.add(Dense(4))
 
     model.summary()
 
@@ -105,9 +72,3 @@ def metric_rmse_percent(y_true, y_pred):
 
     return K.sqrt(K.mean(K.square(error))) / K.mean(K.abs(y_true)) * 100
     # return tf.sqrt(tf.reduce_mean(tf.square(error))) / tf.reduce_mean(tf.abs(y_true)) * 100
-
-
-def rmse_average(array):
-    # weight average of an array with mea and rmse
-
-    return K.sqrt(K.mean(K.square(array)))
