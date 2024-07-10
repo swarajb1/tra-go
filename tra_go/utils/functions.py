1a0c7c21dc56ddbc22109e65a56a6bf5c44cffb1
SWARAJ Mac <bisane.swaraj@gmail.com>
2024-07-10 12:29:14 +0530

    added: sys arguments for with_leverage function

--- tra_go/utils/functions.py
+++ tra_go/utils/functions.py
@@ -1,3 +1,6 @@
+import sys
+
+
 def min_max_scaler(val: float, min_val: float, max_val: float) -> float:
     if max_val == min_val and max_val == 0:
         return 0
@@ -14,3 +17,11 @@ def with_leverage(val: float) -> float:
     per_day: float = (pow(1 + val / 100, 1 / 250) - 1) * 100
 
     return round((pow(per_day * 5 / 100 + 1, 250) - 1) * 100, 2)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        if sys.argv[1] == "with_leverage":
+            print("\n", with_leverage(float(sys.argv[2])))
+        else:
+            print("\n", "Invalid 1st argument")
