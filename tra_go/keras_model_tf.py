43d2795ffce43b4ddd3209a7624a49eb97f5c5e4
SWARAJ Mac <bisane.swaraj@gmail.com>
2024-07-12 00:39:21 +0530

    added: keras model with usage from tensoflow library

---
+++ tra_go/keras_model_tf.py
@@ -0,0 +1,73 @@
+import tensorflow as tf
+from tensorflow.keras.layers import (
+    LSTM,
+    Bidirectional,
+    Dense,
+    Dropout,
+    Input,
+    TimeDistributed,
+)
+from tensorflow.keras.models import Sequential
+
+NUMBER_OF_NEURONS: int = 10
+NUMBER_OF_LAYERS: int = 6
+INITIAL_DROPOUT_PERCENT: float = 0
+
+
+def get_untrained_model(X_train, Y_train):
+    model = Sequential()
+
+    model.add(Input(shape=(X_train[0].shape)))
+
+    for i in range(NUMBER_OF_LAYERS):
+        model.add(
+            Bidirectional(
+                LSTM(
+                    units=NUMBER_OF_NEURONS,
+                    return_sequences=True,
+                    activation="relu",
+                ),
+            ),
+        )
+        #  dropout value decreases in exponential fashion.
+        model.add(Dropout(pow(1 + INITIAL_DROPOUT_PERCENT / 100, 1 / (i + 1)) - 1))
+
+    model.add(TimeDistributed(Dense(units=Y_train[0].shape[1])))
+
+    model.summary()
+
+    print("\n" * 2)
+
+    return model
+
+
+def get_optimiser(learning_rate: float):
+    return tf.keras.optimizers.legacy.Adam(learning_rate=learning_rate)
+
+
+def metric_rmse(y_true, y_pred):
+    # Calculate the root mean squared error (RMSE)
+
+    error = y_true - y_pred
+
+    return tf.sqrt(tf.reduce_mean(tf.square(error)))
+
+
+def metric_abs(y_true, y_pred):
+    # Calculate the absolute mean error (MAE)
+
+    error = y_true - y_pred
+
+    return tf.reduce_mean(tf.abs(error))
+
+
+def metric_abs_percent(y_true, y_pred):
+    error = y_true - y_pred
+
+    return tf.reduce_mean(tf.abs(error)) / tf.reduce_mean(tf.abs(y_true)) * 100
+
+
+def metric_rmse_percent(y_true, y_pred):
+    error = y_true - y_pred
+
+    return tf.sqrt(tf.reduce_mean(tf.square(error))) / tf.reduce_mean(tf.abs(y_true)) * 100
