a8ab69cd5addb0375959c12e8053cf65f074af7c
SWARAJ Mac <bisane.swaraj@gmail.com>
2024-07-29 20:56:25 +0530

    added: functionality to data-loader, wip

--- tra_go/data_loader.py
+++ tra_go/data_loader.py
@@ -8,8 +8,6 @@ from training_zero import (
     NUMBER_OF_POINTS_IN_ZONE_2_ND,
     NUMBER_OF_POINTS_IN_ZONE_DAY,
     TOTAL_POINTS_IN_ONE_DAY,
-    by_date_df_array,
-    data_split_train_test,
 )
 
 from database.enums import (
@@ -37,25 +35,33 @@ class DataLoader:
         self.y_type = y_type
         self.test_size = test_size
 
+        # ----------------------------------------
+
         self.train_y_real_data: NDArray[np.float64]
         self.test_y_real_data: NDArray[np.float64]
 
-        self.load_real_y_data()
+        self.load_data_y_real()
 
-    def load_real_y_data(self) -> None:
-        df = self.get_data_df(ticker_data_type=TickerDataType.CLEANED)
+        self.train_x_data: NDArray[np.float64]
+        self.train_y_data: NDArray[np.float64]
+        self.train_prev_close: NDArray[np.float64]
+
+        self.test_x_data: NDArray[np.float64]
+        self.test_y_data: NDArray[np.float64]
+        self.test_prev_close: NDArray[np.float64]
+
+        self.load_data_train_test_close()
+
+    def load_data_y_real(self) -> None:
+        df = self._get_data_df(ticker_data_type=TickerDataType.CLEANED)
 
         df = self.data_inside_zone(df=df, data_type=RequiredDataType.REAL)
 
-        df_train, df_test = data_split_train_test(
-            df=df,
-            test_size=self.test_size,
-            data_required=RequiredDataType.REAL,
-        )
+        df_train, df_test = self.data_split_train_test(df=df, required_data_type=RequiredDataType.REAL)
 
-        df_train_x, df_train_y = self.data_split_x_y(df=df_train, data_type=RequiredDataType.REAL)
+        df_train_x, df_train_y = self.data_split_x_y(df=df_train, required_data_type=RequiredDataType.REAL)
 
-        df_test_x, df_test_y = self.data_split_x_y(df=df_test, data_type=RequiredDataType.REAL)
+        df_test_x, df_test_y = self.data_split_x_y(df=df_test, required_data_type=RequiredDataType.REAL)
 
         train_y = by_date_df_array(df_train_y, band_type=BandType.BAND_4, io_type=IODataType.OUTPUT_DATA)
         test_y = by_date_df_array(df_test_y, band_type=BandType.BAND_4, io_type=IODataType.OUTPUT_DATA)
@@ -73,7 +79,7 @@ class DataLoader:
 
         return self.train_y_real_data, self.test_y_real_data
 
-    def get_data_df(self, ticker_data_type: TickerDataType) -> pd.DataFrame:
+    def _get_data_df(self, ticker_data_type: TickerDataType) -> pd.DataFrame:
         # file_path: str = f"./data_cleaned/{self.interval.value}/{self.ticker.value} - {self.interval.value}.csv"
 
         file_path = os.path.join(
@@ -87,7 +93,7 @@ class DataLoader:
 
         return df
 
-    def data_split_x_y(self, df: pd.DataFrame, data_type: RequiredDataType | None) -> pd.DataFrame:
+    def data_split_x_y(self, df: pd.DataFrame, required_data_type: RequiredDataType | None) -> pd.DataFrame:
         """Splits the data into input, output dataframe."""
 
         df_i = pd.DataFrame()
@@ -117,7 +123,7 @@ class DataLoader:
             BandType.BAND_5: ["open", "high", "low", "close", "volume"],
         }
 
-        if data_type == RequiredDataType.REAL:
+        if required_data_type == RequiredDataType.REAL:
             columns_x = band_columns[BandType.BAND_4]
             columns_y = band_columns[BandType.BAND_4]
         else:
@@ -145,7 +151,11 @@ class DataLoader:
         elif NUMBER_OF_POINTS_IN_ZONE_2_ND == 150:
             initial_index_offset = 36
 
-        number_of_days = len(df) // TOTAL_POINTS_IN_ONE_DAY
+        number_of_days: int = len(df) // TOTAL_POINTS_IN_ONE_DAY
+
+        assert (
+            len(df) % TOTAL_POINTS_IN_ONE_DAY == 0
+        ), "Full Cleaned Data length is not divisible by TOTAL_POINTS_IN_ONE_DAY"
 
         for day in range(number_of_days):
             start_index: int = day * TOTAL_POINTS_IN_ONE_DAY + initial_index_offset
@@ -155,16 +165,111 @@ class DataLoader:
 
         res_df.reset_index(drop=True, inplace=True)
 
-        base_columns: list[str] = ["open", "high", "low", "close", "volume"]
-
-        columns: list[str]
-
-        if data_type == RequiredDataType.TRAINING:
-            columns = base_columns + ["real_close"]
-        elif data_type == RequiredDataType.REAL:
-            columns = base_columns
+        columns: list[str] = self.get_columns(data_type)
 
         return res_df[columns]
+
+    def get_columns(self, required_data_type: RequiredDataType) -> list[str]:
+        columns: list[str]
+
+        base_columns: list[str] = ["open", "high", "low", "close", "volume"]
+
+        if required_data_type == RequiredDataType.TRAINING:
+            columns = base_columns + ["real_close"]
+        elif required_data_type == RequiredDataType.REAL:
+            columns = base_columns
+
+        return columns
+
+    def data_split_train_test(
+        self,
+        df: pd.DataFrame,
+        required_data_type: RequiredDataType,
+    ) -> tuple[pd.DataFrame, pd.DataFrame]:
+        # split into train and test
+
+        assert (
+            len(df) % NUMBER_OF_POINTS_IN_ZONE_DAY == 0
+        ), "Full Zone Data length is not divisible by NUMBER_OF_POINTS_IN_ZONE_DAY"
+
+        number_of_days: int = len(df) // NUMBER_OF_POINTS_IN_ZONE_DAY
+
+        train_days: int = int(number_of_days * (1 - self.test_size))
+
+        first_test_index: int = train_days * NUMBER_OF_POINTS_IN_ZONE_DAY
+
+        train_df = df.iloc[:first_test_index]
+        test_df = df.iloc[first_test_index:]
+
+        train_df.reset_index(drop=True, inplace=True)
+        test_df.reset_index(drop=True, inplace=True)
+
+        columns: list[str] = self.get_columns(required_data_type)
+
+        return train_df[columns], test_df[columns]
+
+    def get_train_test_split(
+        self,
+    ) -> tuple[
+        tuple[NDArray[np.float64], NDArray[np.float64], NDArray[np.float64]],
+        tuple[NDArray[np.float64], NDArray[np.float64], NDArray[np.float64]],
+    ]:
+        return (
+            (self.train_x_data, self.train_y_data, self.train_prev_close),
+            (self.test_x_data, self.test_y_data, self.test_prev_close),
+        )
+
+    def load_data_train_test_close(self, df: pd.DataFrame) -> pd.DataFrame:
+        """Splits the data into input, output dataframe, and the previous close
+        price dataframe."""
+
+        df = self.data_inside_zone(df=df, data_type=RequiredDataType.TRAINING)
+
+        df_i = pd.DataFrame()
+        df_o = pd.DataFrame()
+
+        prev_close = pd.DataFrame()
+
+        number_of_days: int = len(df) // NUMBER_OF_POINTS_IN_ZONE_DAY
+
+        for day in range(number_of_days):
+            day_start_index: int = int(day * NUMBER_OF_POINTS_IN_ZONE_DAY)
+            day_end_index: int = day_start_index + NUMBER_OF_POINTS_IN_ZONE_DAY - 1
+
+            first_2_nd_zone_index: int = int(day_start_index + NUMBER_OF_POINTS_IN_ZONE_1_ST)
+
+            df_i = pd.concat([df_i, df.iloc[day_start_index:first_2_nd_zone_index]])
+            df_o = pd.concat([df_o, df.iloc[first_2_nd_zone_index : day_end_index + 1]])
+
+            dict_1 = {"real_close": df.iloc[day_start_index, df.columns.get_loc("real_close")]}
+
+            prev_close = pd.concat([prev_close, pd.DataFrame(dict_1, index=[0])], ignore_index=True)
+
+        df_i.reset_index(drop=True, inplace=True)
+        df_o.reset_index(drop=True, inplace=True)
+
+        columns_x: list[str]
+        columns_y: list[str]
+
+        if self.x_type == BandType.BAND_4:
+            columns_x = ["open", "high", "low", "close"]
+        elif self.x_type == BandType.BAND_2:
+            columns_x = ["low", "high"]
+        elif self.x_type == BandType.BAND_5:
+            columns_x = ["open", "high", "low", "close", "volume"]
+
+        if self.y_type == BandType.BAND_4:
+            columns_y = ["open", "high", "low", "close"]
+        elif self.y_type in [BandType.BAND_2, BandType.BAND_2_1]:
+            columns_y = ["low", "high"]
+        elif self.y_type == BandType.BAND_5:
+            columns_y = ["open", "high", "low", "close", "volume"]
+
+        return (
+            df_i[columns_x],
+            df_o[columns_y],
+            prev_close[["real_close"]],
+        )
 
 
 def check_gaps(data: NDArray[np.float64]) -> None:
@@ -185,3 +290,31 @@ def check_gaps(data: NDArray[np.float64]) -> None:
         "{:.2f}".format(count_gaps_train / (data.shape[0] * data.shape[1]) * 100),
         " %\n",
     )
+
+
+def by_date_df_array(df: pd.DataFrame, band_type: BandType, io_type: IODataType) -> NDArray:
+    array = df.values
+
+    points_in_each_day: int
+
+    if io_type == IODataType.INPUT_DATA:
+        points_in_each_day = NUMBER_OF_POINTS_IN_ZONE_1_ST
+    elif io_type == IODataType.OUTPUT_DATA:
+        points_in_each_day = NUMBER_OF_POINTS_IN_ZONE_2_ND
+
+    if band_type == BandType.BAND_4:
+        assert len(array) % (points_in_each_day * 4) == 0, "Array length is not divisible by 4 * points_in_each_day"
+
+        res = array.reshape(len(array) // points_in_each_day, points_in_each_day, 4)
+
+    elif band_type in [BandType.BAND_2, BandType.BAND_2_1]:
+        assert len(array) % (points_in_each_day * 2) == 0, "Array length is not divisible by 2 * points_in_each_day"
+
+        res = array.reshape(len(array) // points_in_each_day, points_in_each_day, 2)
+
+    elif band_type == BandType.BAND_5:
+        assert len(array) % (points_in_each_day * 5) == 0, "Array length is not divisible by 5 * points_in_each_day"
+
+        res = array.reshape(len(array) // points_in_each_day, points_in_each_day, 5)
+
+    return res
