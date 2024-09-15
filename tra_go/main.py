55c271de01c809309ac35dbdddb46b8690eabb14
SWARAJ Mac <bisane.swaraj@gmail.com>
2024-09-15 20:12:41 +0530

    added: 2 more metric for ModelCheckpoint

--- tra_go/main.py
+++ tra_go/main.py
@@ -99,14 +99,14 @@ def main_training():
     mcp_save_3 = ModelCheckpoint(
         f"{checkpoint_path_prefix} - modelCheckPoint-3.keras",
         save_best_only=True,
-        monitor="metric_win_pred_capture_percent",
+        monitor="metric_win_pred_capture_total_percent",
         mode="max",
     )
 
     mcp_save_4 = ModelCheckpoint(
         f"{checkpoint_path_prefix} - modelCheckPoint-4.keras",
         save_best_only=True,
-        monitor="val_metric_win_pred_capture_percent",
+        monitor="val_metric_win_pred_capture_total_percent",
         mode="max",
     )
 
@@ -124,6 +124,34 @@ def main_training():
         mode="max",
     )
 
+    mcp_save_7 = ModelCheckpoint(
+        f"{checkpoint_path_prefix} - modelCheckPoint-7.keras",
+        save_best_only=True,
+        monitor="metric_try_1",
+        mode="max",
+    )
+
+    mcp_save_8 = ModelCheckpoint(
+        f"{checkpoint_path_prefix} - modelCheckPoint-8.keras",
+        save_best_only=True,
+        monitor="val_metric_try_1",
+        mode="max",
+    )
+
+    mcp_save_9 = ModelCheckpoint(
+        f"{checkpoint_path_prefix} - modelCheckPoint-9.keras",
+        save_best_only=True,
+        monitor="metric_loss_comp_2",
+        mode="min",
+    )
+
+    mcp_save_10 = ModelCheckpoint(
+        f"{checkpoint_path_prefix} - modelCheckPoint-10.keras",
+        save_best_only=True,
+        monitor="val_metric_loss_comp_2",
+        mode="min",
+    )
+
     callbacks = [
         tensorboard_callback,
         terNan,
@@ -133,6 +161,10 @@ def main_training():
         mcp_save_4,
         mcp_save_5,
         mcp_save_6,
+        mcp_save_7,
+        mcp_save_8,
+        mcp_save_9,
+        mcp_save_10,
     ]
 
     print(f"\n\nnow_datetime:\t{now_datetime}\n\n")
@@ -220,7 +252,7 @@ def main_training():
     print("training y data shape\t", Y_train.shape)
 
     print("model input shape\t", model.input_shape)
-    print("model output shape\t", model.output_shape)
+    print("model output shape\t", model.output_shape, "\n" * 2)
 
     history = model.fit(
         x=X_train,
@@ -238,7 +270,7 @@ def main_training():
     print(f"\n\nnow_datetime:\t{now_datetime}\n\n")
     print("-" * 30)
 
-    # number_of_model_checkpoints: int = 6
+    # number_of_model_checkpoints: int = 10
 
     # num_models_to_evaluate: int = number_of_model_checkpoints + 1
 
@@ -279,8 +311,6 @@ def main():
 
     assert_env_vals()
 
-    suppress_cpu_usage()
-
     if len(sys.argv) > 1:
         number_of_models: int = 7
         move_files: bool = False
@@ -289,13 +319,19 @@ def main():
             number_of_models = int(sys.argv[2])
 
         if len(sys.argv) > 3:
-            move_files = bool(int(sys.argv[3]))
+            if sys.argv[3] not in ["true", "false"]:
+                raise ValueError("3rd argument should be either 'true' or 'false'")
+
+            move_files = sys.argv[3] == "true"
 
         if sys.argv[1] == "true":
             global TICKER
 
+            suppress_cpu_usage()
+
             for ticker in list_of_tickers:
                 TICKER = ticker
+
                 main_training()
 
         elif sys.argv[1] == "training_new":
